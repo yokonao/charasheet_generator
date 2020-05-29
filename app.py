170ea6c696ef657ecd3bb6caf4de4bf15eabac8e
横塚直 <yokotsukanao@yokotsukatadashinoMacBook-Air.local>
2020-05-29 16:52:28 +0900

    third commit

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 import json
 import ast
 import urllib
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template
 from main import make
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/")
+def menu():
+    return render_template("index.html")
+
+
+@app.route('/pdf', methods=['GET', 'POST'])
 def hello():
     if request.method == 'GET':
-        with open('chara.json') as f:
+        with open('resources/empty.json') as f:
             data_dict = json.load(f)
     else:
         # decoding binary
         data_str = request.get_data().decode('utf-8')
         # convertion to dictionary
         data_dict = ast.literal_eval(data_str)
+
     # saving temporarily PDF file to buffer
     buffer = make(data_dict)
     pdf = buffer.getvalue()
@@ -24,7 +30,7 @@ def hello():
     # generating response
     response = make_response(pdf)
     # deciding the file name from chara name
-    downloadFileName = data_dict['name']
+    downloadFileName = data_dict['name'] if data_dict['name'] else '名無しの権兵衛'
     dispostion = "attachment; filename*=UTF-8''"
     # enable encoding Japanese
     dispostion += urllib.parse.quote(downloadFileName + '.pdf')
